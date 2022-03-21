62cc0cc70bec7671fedf09cde1c266825b2bed93
alecvipa <hebrit_626@hotmail.com>
2022-03-21 17:34:12 -0600

    finished site

--- server.js
+++ server.js
@@ -64,11 +64,11 @@ app.post("/ajax/email", function (request, response) {
     });
 
     
-    var htmlBody = `<h2>Correo de contacto</h2><p>Nombre: ${request.body.name} </p><p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Número de contacto:${request.body.number} </p>`;
+    var htmlBody = `<h2>Correo de contacto</h2><p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p>`;
     var mail = {
         from: '"Team: Xyncs Web Studio',
         to: 'hebrit_626@hotmail.com',
-        subject: '¡Alguien ha dejado sus datos en Frecuencia Proveedora!',
+        subject: '¡Alguien ha dejado sus datos en Consulting Jofer!',
         html: htmlBody
     };
     transporter.sendMail(mail, function (err, info) {
